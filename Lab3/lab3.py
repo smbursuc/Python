e77ebe804cdcc15a43e0eef9e600d5aa91c8ea92
BursucSerban-Mihai <godares104@gmail.com>
2022-10-28 07:42:55 +0300

    lab3 corect (modificat ieri)

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -27,7 +27,7 @@ def ex_2():
     
     print(functie_ex2(String))
 
-def ex_3():
+def ex_3(): #
 
     dict1 = {'A':1,'B':2,'C': {'D':1,'D':2}}
     dict1 = dict(dict1)
@@ -53,13 +53,27 @@ def ex_3():
         print("dict1 keys",dict1_keys)
         print("dict2 val",dict2_values)
         print("dict2 keys",dict2_keys)
-        if dict1_keys == dict2_keys and dict1_values == dict2_values:
-            return True
-        else:
-            return False
+
+        tuples = []
+
+        for key in dict1.keys():
+            tuples.append((key,dict1[key]))
+        
+        for key in dict2.keys():
+            tuples.append((key,dict2[key]))
+        
+        #tuples = sorted(tuples, key=lambda x:x[0])
+
+        for tuple in tuples:
+            if tuples.count(tuple)!=2:
+                return False
+        
+        return True
     
     print(functie_ex3(dict1,dict2))
     print(functie_ex3(dict1,dict3))
+
+#ex_3()
 
 def ex_4():
 
@@ -75,10 +89,10 @@ def ex_4():
     
     print(build_xml_element("a", "Hello there", href = "http://python.org", _class= "my-link", id = "someid"))
 
-def ex_5():
+def ex_5(): #
 
-    rules = {("key1", "", "inside", ""),("key2", "start", "middle", "winter")}
-    dictionary = {"key1": "come inside, it's too cold out","key3": "this is not valid"}
+    rules = {("key1", "", "inside", "")}
+    dictionary = {"key1": "inside it's too cold out"}
     def validate_dict(rules,dictionary):
         for rule in rules:
             if rule[0] not in dictionary:
@@ -86,25 +100,32 @@ def ex_5():
             
             value = dictionary[rule[0]]
 
-            if rule[1] == "" and rule[3] != "":
-                if value.count(rule[2]) == 0 or rule[3] != value[-1]:
-                    return False
+            split_value = value.split(" ")
+
+            spotted = 0
+            for i in range(1,len(split_value)-1):
+                if split_value[i] == rule[2]:
+                    spotted = 1
             
-            elif rule[3] == "" and rule[1] != "":
-                if value.count(rule[2]) == 0 or rule[1] != value[0]:
-                    return False
-            
-            elif rule[3] == "" and rule[1] == "":
-                if value.count(rule[2]) == 0:
-                    return False
-            
-            elif rule[1] != value[0] or rule[3] != value[-1] or value.count(rule[2]) == 0:
+            if spotted == 0:
                 return False
+            
+            if rule[1] == '':
+                if split_value[len(split_value)-1] != rule[3]:
+                    return False
+            elif rule[3] == '':
+                if split_value[0] != rule[1]:
+                    print('a')
+                    return False
+            elif rule[1] != split_value[0] or rule[3] != split_value[len(split_value)-1]:
+                return False
+
+
         return True
     
     print(validate_dict(rules,dictionary))
-            
-            
+
+ex_5()
 def ex_6():
 
     def functie_ex6(lista):
@@ -160,8 +181,9 @@ def ex_7():
     
     print(functie_ex7({1,2},{2,3}))
 
-def ex_8():
+def ex_8(): #
     
+    #mapping = {'start': 'a', 'b': 'a', 'a': '6', '6': 'z', 'x': '2', 'z': 'x', '2': 'y', 'y': 'start'} loop
     mapping = {'start': 'a', 'b': 'a', 'a': '6', '6': 'z', 'x': '2', 'z': '2', '2': '2', 'y': 'start'}
     def loop(mapping):
         visited = []
@@ -169,15 +191,17 @@ def ex_8():
         visited.append(mapping[key])
         key = mapping['start'] 
         while True:
+            if mapping[key] in visited and mapping[key]!='start':
+                break
             visited.append(mapping[key])
             if visited[len(visited)-1] == visited[len(visited)-2]:
                 break
             key = mapping[key]
         
-        visited.pop()
         return visited
     
     print(loop(mapping))
+
 
 def ex_9():
 
@@ -194,7 +218,6 @@ def ex_9():
     
     print(functie_ex9(1, 2, 3, 4, x=1, y=2, z=3, w=5))
 
-ex_9()
 
 
 
