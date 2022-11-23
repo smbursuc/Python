364105eec071eb18917b709301c99a0e640d993f
BursucSerban-Mihai <79265695+smbursuc@users.noreply.github.com>
2022-11-23 02:02:44 +0200

    lab 6

--- Lab6/lab6.py
+++ Lab6/lab6.py
@@ -1,16 +1,21 @@
 import re
+import exrex
+import os
 def ex_1():
 
     def functie_ex1(text):
-        result = []
-        split_text = text.split(" ")
-        r = re.compile('^[a-zA-Z0-9]+$')
-        for txt in split_text:
-            if r.match(txt):
-                result.append(txt)
-        return result
+        # Regex that checks if the string is a sentence
+        regex = "^([A-Za-z0-9]+ [A-Za-z0-9]+)+$"
+        r = re.compile(regex)
+        #print(exrex.getone(regex))
+        if r.match(text):
+            print("Este propozitie")
+        else:
+            print("Nu este propozitie")
+            
+        
     
-    print(functie_ex1("hello19 20 world"))
+    functie_ex1("hello19 20 world")
 
 def ex_2():
 
@@ -47,26 +52,100 @@ def ex_4():
 
     def functie_ex4(attrs):
         result = []
-        # regex = ""
-        # for key,value in attrs.items():
-        #     string = f"{key}" + "=" + f"\"{value}\""
-        #     regex = regex + string
-        # print(regex)
         f = open("my_xml.txt",'r')
         data = f.read().splitlines()
         for line in data:
             good = 1
             for key,value in attrs.items():
-                r = re.compile(f"{key}" + "=" + f"\"{value}\"")
-                if r.search(line):
+                keyval = f"{key}" + "=" + f"\"{value}\""
+                regex = f'(<.* {keyval} .*((/>)|>))|(<.* {keyval}((/>)|>))'
+                r = re.compile(regex)
+                if not r.match(line):
                     good = 0
+            if good == 1:
+                result.append(line)
             
         return result
 
     
     print(functie_ex4({"class":"python","name":"python"}))
 
-ex_4()
+def ex_5():
+    def functie_ex5(attrs):
+        result = []
+        f = open("my_xml.txt",'r')
+        data = f.read().splitlines()
+        for line in data:
+            for key,value in attrs.items():
+                keyval = f"{key}" + "=" + f"\"{value}\""
+                regex = f'(<.* {keyval} .*((/>)|>))|(<.* {keyval}((/>)|>))'
+                r = re.compile(regex)
+                #print(exrex.getone(regex))
+                if r.match(line):
+                    result.append(line)
+                    break
+            
+        return result
+
+    
+    print(functie_ex5({"class":"python","fake":"fake"}))
 
 
-#print("serban"[0:])
+def ex_6():
+
+    def censor(matchObj):
+        text = matchObj.group(0)
+        cenzurat = ''
+        for i in range(len(text)):
+            if i % 2 == 0:
+                cenzurat += '*'
+            else:
+                cenzurat += text[i]
+        return cenzurat
+    def functie_ex6(text):
+        print(re.sub('[aeiouAEIOU][a-zA-Z0-9][aeiouAEIOU]', censor, text))
+    
+    functie_ex6("hello world ana are")
+
+def ex_7():
+
+    def functie_ex7(cnp):
+        regex = "^[1-8][0-9][0-9](0[1-9]|[0-9][0-9])(0[1-9]|[0-9][0-9])(0[1-9]|[0-9][0-9])(00[1-9]|[0-9][0-9][0-9])[0-9]$"
+        #10335875
+        r = re.compile(regex)
+        print(exrex.getone(regex))
+        if r.match(cnp):
+            print("CNP valid")
+        else:
+            print("CNP invalid")
+
+    functie_ex7("1971027265798")
+
+def ex_8():
+
+    def generate_substrings(string):
+        result = []
+        for i in range(len(string)):
+            for j in range(i,len(string)):
+                result.append(string[i:j+1])
+        return result
+
+    def functie_ex8(dir,regex):
+        result = []
+        for dirs, subdirs, root in os.walk(dir):
+            for file in root:
+                file = file.split('.')[0]
+                substrings = generate_substrings(file)
+                found = 0 
+                for substring in substrings:
+                    r = re.compile(regex)
+                    if r.match(substring):
+                        found=1
+                        print(">> " + file)
+                        break
+                if found == 0:
+                    print(file)
+    functie_ex8(".",'^([a-zA-Z0-9]+_[a-zA-Z0-9]+)$')
+
+
+ex_8()
